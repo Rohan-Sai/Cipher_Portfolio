2985f4a475faebd405c12063428394e06f474017
Rohan Sai <rohansai231098@gmail.com>
2020-08-30 21:48:44 +0530

    Updates

---
+++ precache-manifest.7fc5d7037c93bd9bb575ebdfc1cdb477.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dfb7c28df2ac6e30bc9",
+    "url": "/Cipher_Portfolio/static/css/main.ea21c0d7.chunk.css"
+  },
+  {
+    "revision": "1dfb7c28df2ac6e30bc9",
+    "url": "/Cipher_Portfolio/static/js/main.1dfb7c28.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/Cipher_Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "0e721e7557af030d5025",
+    "url": "/Cipher_Portfolio/static/js/runtime~main.0e721e75.js"
+  },
+  {
+    "revision": "44dde8af884d85706a05e73baa4b347d",
+    "url": "/Cipher_Portfolio/index.html"
+  }
+];
