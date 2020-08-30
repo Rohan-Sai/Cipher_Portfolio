a324794a3e91110924c08e36b22c077744c7e173
Rohan Sai <rohansai231098@gmail.com>
2020-08-30 21:44:25 +0530

    Finaol Commit

---
+++ src/resumeData.js
@@ -0,0 +1,63 @@
+let resumeData = {
+    "name": "D.Rohan Sai",
+    "role": "Front-end Developer",
+    "linkedinId":"rohan-sai-aa8277152/",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rohan-sai-aa8277152/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Rohan-Sai",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently a final year student at Lovely Professional University and pursuing my B.Tech from here.I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/Rohan-Sai",
+    "education":[
+      {
+        "UniversityName":"Lovely Professional University",
+        "specialization":"Computer Science",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+      },
+    ],
+    "work":[
+      {
+        "CompanyName":"JP Morgan and Co",
+        "specialization":"Virtual Software Engineering",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2020",
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"C++"
+      },
+      {
+        "skillname":"Java"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"weWork CTracker",
+        "description":"Website",
+      },
+    ],
+  }
+  
+  export default resumeData
